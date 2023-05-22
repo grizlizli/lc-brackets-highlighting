500f3c83ebff212d6cb62962766d7263f0d483ee
Miloš Krstić <mrkrsticmilos@gmail.com>
2023-05-22 15:50:29 +0200

    task description and examples displayed in the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+const TASK_DESCRIPTION =
+  "Implement an editable text block where words wrapped in square brackets are highlighted in blue, and words wrapped in curly brackets are highlighted in green. Overlapping patterns should be ignored, they shouldn't be highlighted. The component should be implemented in a separate module of Angular application. The application should be published on GitHub in a public repository. ";
+
+const TASK_EXAMPLES =
+  'For example: [Hello/Hi/Greetings] {Name}, how are you? // [Hello/Hi/Greetings] and {Name} are correct patterns [Hello {Surname}/Hi/Greetings] {Name [...] Surname}, how are you? // These patterns are incorrect';
+
 @Component({
   selector: 'lc-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Launch Control Brackets Highlighting';
+
+  taskDescription = TASK_DESCRIPTION;
+  taskExamples = TASK_EXAMPLES;
 }
