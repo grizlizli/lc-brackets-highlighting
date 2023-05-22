d4771007ccf0e9f7dc3f988edf187ee68b037506
Miloš Krstić <mrkrsticmilos@gmail.com>
2023-05-22 15:38:01 +0200

    app component should render title and text edit component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TextEditComponent } from './components/text-edit/text-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextEditComponent
   ],
   imports: [
     BrowserModule
